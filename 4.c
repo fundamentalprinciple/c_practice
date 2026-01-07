02ad2bf4b6f923aab83f88a5731faefdec221545
fundamentalprinciple <princedixit931@gmail.com>
2026-01-07 07:41:16 +0000

    structs

--- 4.c
+++ 4.c
@@ -3,7 +3,7 @@
 int main() {
 	int age = 20;
 	int *pAge = &age;
-	
+	int num = 34;	
 	/*
 	char str[] = "OYE!";
 	char *pStr = &str;
@@ -11,6 +11,7 @@ int main() {
 	
 	printf("%p\n", pAge);	
 	printf("%p\n", &pAge);
+	printf("%p\n", &num);
 	printf("%d\n", *pAge);
 	printf("%lu\n", sizeof(age));
 	printf("%lu\n", sizeof(pAge));
