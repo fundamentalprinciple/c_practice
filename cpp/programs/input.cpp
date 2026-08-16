051b4ff347b9017c178d9f8e2507cf583a948be1
fundamentalprinciple <princedixit931@gmail.com>
2026-08-16 16:23:30 +0530

    add cpp files

---
+++ cpp/programs/input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  string x;	//std:string
+  cout << "Type anything: \n";
+  cin >> x;
+  cout << "You typed: " << x << endl;
+  return 0;
+}
---
+++ cpp/programs/vars.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int apples = 2;
+  cout << "I ate " << apples << " apples today\n";
+  return 0;
+}
